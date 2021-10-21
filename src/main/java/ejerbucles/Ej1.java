6ed6101223ca00dc86d74c395d8496408fc000f6
Juan José Bazán Espinosa <eltiburonjuanjo@hotmail.com>
2021-10-21 10:45:05 +0200

    añade pom.xml y ej1.java

---
+++ src/main/java/ejerbucles/Ej1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerbucles;
+
+/**
+ *
+ * @author ajbazan01
+ */
+public class Ej1 {
+
+    
+    public static void main(String[] args) {
+        int x =1;
+        
+        while (x<100) {
+            System.out.println(x);
+            System.out.println(" - ");
+            x = x + 1;
+        }
+            
+        
+    }
+    
+}
